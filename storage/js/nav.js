ad7a365888d508a960a5e32b98442fa8a022531f
US4 <mangokirby89@gmail.com>
2025-05-06 19:14:10 -0400

    Update nav.js

--- storage/js/nav.js
+++ storage/js/nav.js
@@ -1,7 +1,5 @@
-// Find the element with the class 'navbar'
 const navbar = document.querySelector('.navbar');
 
-// If the navbar exists, remove it and its children
 if (navbar) {
     navbar.remove();
 }
