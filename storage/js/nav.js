41e9a4672f2a57d0a00f4986a3ae773e823210b1
US4 <mangokirby89@gmail.com>
2025-05-06 18:46:12 -0400

    Update nav.js

--- storage/js/nav.js
+++ storage/js/nav.js
@@ -1,15 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const navButtons = `
-    <a href="/"><i class="fa fa-home"></i> </a>
-    <a href="/pages/games"><i class="fas fa-gamepad"></i> </a>
-    <a href="/pages/media"><i class="fa-solid fa-video"></i> </a>
-    <a href="/pages/apps"><i class="fa-sharp fa-solid fa-layer-group"></i> </a>
-    <a href="/pages/prox"><i class="fas fa-globe"></i> </a>
-    <a href="/pages/partners"><i class="fa-solid fa-users"></i> </a>
-    <a href="/pages/credits"><i class="fa-solid fa-book"></i> </a>
-    <a href="/pages/extra"><i class="fas fa-plus"></i> </a>
-    <a href="/pages/settings"><i class="fa-solid fa-gear"></i> </a>
-    `;
-
-    document.querySelector('.right-side').innerHTML = navButtons;
-});
+document.querySelectorAll('.navbar').forEach(el => el.classList.remove('navbar'));
