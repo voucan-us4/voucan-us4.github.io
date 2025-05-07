b7e096c45e8a4fc2a9019b24df423edec6a06b75
US4 <mangokirby89@gmail.com>
2025-05-07 07:19:46 -0400

    Update splash-messages.js

--- storage/js/splash-messages.js
+++ storage/js/splash-messages.js
@@ -8,12 +8,11 @@ const phrases = [
     `im in your walls`,
     `"play the strongest battlegrounds" - literalaj`,
     /*{ other: 'loop', width: '200px', type: "video", src: `https://media.tenor.com/SIpmtvnEsDIAAAPo/rotating-chips.mp4` },*/
-    { type: "image", width: '50%', src: `https://i.ibb.co/7tj7Fsdg/image.png` },
+    { type: "image", width: '100%', src: `https://i.ibb.co/7tj7Fsdg/image.png` },
     `jackson is batman`,
     `jacob is the WORST violin. Make sure to bully him (8th grade violin, probably 400 pounds tbh)`,
     /*{ type: "image", width: '400px', src: `https://c.tenor.com/9MVlipGuNioAAAAC/tenor.gif` },*/
     `"im the best kahoot player" - Jay C`,
-    `can someone sign my yearbook, rm 123 at the end of the day`
 ];
 
 const paragraph = document.getElementById('dynamicParagraph');
