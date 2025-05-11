ece4e0999655195aab712207ace6df8af6ebce38
US4 <mangokirby89@gmail.com>
2025-05-11 11:56:03 -0400

    Update ads.js

--- storage/js/ads.js
+++ storage/js/ads.js
@@ -1,9 +1,12 @@
-  let clickCount = localStorage.getItem(window.location.hostname + "_clickedAds") || 0;
-  
-  clickCount = parseInt(clickCount);
+let clickCount = localStorage.getItem(window.location.hostname + "_clickedAds") || 0;
 
-  if (clickCount < 10) {
-    const adIframe = document.getElementById('container-9bd6dd1837226b9fe69dcbb4f296d85a');
+clickCount = parseInt(clickCount);
+
+if (clickCount < 10) {
+  const tabIframe = document.querySelector('.tab-iframe.active'); // Select the active iframe
+
+  if (tabIframe) {
+    const adIframe = tabIframe.contentWindow.document.getElementById('container-9bd6dd1837226b9fe69dcbb4f296d85a');
 
     if (adIframe) {
       adIframe.addEventListener('click', function() {
@@ -14,7 +17,7 @@
         if (clickCount >= 10) {
           adIframe.style.display = 'none';
           
-          const scriptElements = document.querySelectorAll('script[src="//pl26618098.profitableratecpm.com/9b/d6/dd/9bd6dd1837226b9fe69dcbb4f296d85a.js"]');
+          const scriptElements = tabIframe.contentWindow.document.querySelectorAll('script[src="//pl26618098.profitableratecpm.com/9b/d6/dd/9bd6dd1837226b9fe69dcbb4f296d85a.js"]');
           scriptElements.forEach(script => script.remove());
 
           console.log('10th click');
@@ -24,7 +27,8 @@
 
     const script = document.createElement('script');
     script.src = '//pl26618098.profitableratecpm.com/9b/d6/dd/9bd6dd1837226b9fe69dcbb4f296d85a.js';
-    document.body.appendChild(script);
-  } else {
-    console.log('10th click');
+    tabIframe.contentWindow.document.body.appendChild(script);
   }
+} else {
+  console.log('10th click');
+}
