c89e74ccb64fd7e5fb7aed1840b127833c124fa2
US4 <mangokirby89@gmail.com>
2025-05-11 13:03:34 -0400

    Update ads.js

--- storage/js/ads.js
+++ storage/js/ads.js
@@ -4,11 +4,15 @@ if (localStorage.getItem("Ads") === null) {
 
 setInterval(function() {
     if (localStorage.getItem('Ads') === 'true') {
-        if (!document.querySelector('script[src="//pl26618098.profitableratecpm.com/9b/d6/dd/9bd6dd1837226b9fe69dcbb4f296d85a.js"]')) {
-            var script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = '//pl26618098.profitableratecpm.com/9b/d6/dd/9bd6dd1837226b9fe69dcbb4f296d85a.js';
-            document.head.appendChild(script);
+        var iframe = document.querySelector('.tab-iframe.active');
+        if (iframe && iframe.contentWindow) {
+            var iframeDoc = iframe.contentWindow.document;
+            if (!iframeDoc.querySelector('script[src="//pl26618098.profitableratecpm.com/9b/d6/dd/9bd6dd1837226b9fe69dcbb4f296d85a.js"]')) {
+                var script = iframeDoc.createElement('script');
+                script.type = 'text/javascript';
+                script.src = '//pl26618098.profitableratecpm.com/9b/d6/dd/9bd6dd1837226b9fe69dcbb4f296d85a.js';
+                iframeDoc.head.appendChild(script);
+            }
         }
     }
 }, 500);
