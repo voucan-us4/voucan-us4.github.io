e0cf2b243ed9f2ebda40264f9610e296bc74beb9
US4 <mangokirby89@gmail.com>
2025-05-11 12:45:39 -0400

    Update ads.js

--- storage/js/ads.js
+++ storage/js/ads.js
@@ -1,8 +1,13 @@
 if (localStorage.getItem("Ads") === null) {
     localStorage.setItem("Ads", "true");
 }
-
 if (localStorage.getItem("Ads") === "false") {
-    const script = document.querySelector("script[src='//pl26618098.profitableratecpm.com/9b/d6/dd/9bd6dd1837226b9fe69dcbb4f296d85a.js']");
-    if (script) script.remove();
+    const iframe = document.querySelector(".tab-iframe");
+
+    if (iframe) {
+        const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+        const script = iframeDoc.querySelector("script[src='//pl26618098.profitableratecpm.com/9b/d6/dd/9bd6dd1837226b9fe69dcbb4f296d85a.js']");
+        if (script) script.remove();
+    }
 }
