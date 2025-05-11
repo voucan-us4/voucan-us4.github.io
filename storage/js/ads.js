fe9d0464924d63abbd6fc3557bbc125afa22109f
US4 <mangokirby89@gmail.com>
2025-05-11 12:26:43 -0400

    Update ads.js

--- storage/js/ads.js
+++ storage/js/ads.js
@@ -1,3 +1,7 @@
+if (localStorage.getItem("Ads") === null) {
+    localStorage.setItem("Ads", "true");
+}
+
 if (localStorage.getItem("Ads") === "false") {
     const script = document.querySelector("script[src='//pl26618098.profitableratecpm.com/9b/d6/dd/9bd6dd1837226b9fe69dcbb4f296d85a.js']");
     if (script) script.remove();
