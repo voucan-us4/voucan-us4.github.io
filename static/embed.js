2263ff1461991bc18b953717b30338e8ad583168
US4 <mangokirby89@gmail.com>
2025-05-13 14:51:40 -0400

    Update embed.js

--- static/embed.js
+++ static/embed.js
@@ -1,20 +1,6 @@
-"use strict";
-let destination = "";
-
-try {
-  destination = new URL(location.hash.slice(1)).toString();
-} catch (err) {
-  alert(`Bad # string or bad URL. Got error:\n${err}`);
-  throw err;
-}
-
-registerSW()
-  .then(() => {
-    window.open(
-      __uv$config.prefix + __uv$config.encodeUrl(destination),
-      "_self"
-    );
-  })
-  .catch((err) => {
-    alert(`Encountered error:\n${err}`);
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  const link = new URLSearchParams(window.location.search).get('p');
+  if (link) {
+    window.location.href = `/static/load/${__uv$config.encodeUrl(link)}`;
+  }
+});
