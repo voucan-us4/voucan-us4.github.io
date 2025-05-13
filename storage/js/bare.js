f30a7c5defb00f8aee12c60ba77facdaa35cd9cb
US4 <mangokirby89@gmail.com>
2025-05-13 17:38:17 -0400

    Delete storage/js/bare.js

--- storage/js/bare.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-	const CB = localStorage.getItem('custombare');
-	if (CB && self.__uv$config) {
-		self.__uv$config.bare = CB;
-		console.log('bare set to', CB);
-	} else {
-		console.warn('No custom bare set in localStorage ornot defined.');
-	}
-});
