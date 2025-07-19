b51d61c369dbb97e7b03c8172698fb0b7112a089
voucan <vcn4313@gmail.com>
2025-07-19 15:52:44 -0400

    Update notify.js

--- storage/js/notify.js
+++ storage/js/notify.js
@@ -75,5 +75,5 @@ function createNotification(message) {
 }
 
 window.onload = () => {
-    createNotification('US5 will be releasing in the 2025-2026 school year - Server boosters will now get early access to US5! - Join our discord for updates on US5: https://discord.gg/VDA8Ngx38Z')
+    createNotification('US4 is now discontinued. We are currently working on US5. Export your data at `/pages/data` on this site. https://discord.gg/VDA8Ngx38Z')
 };
